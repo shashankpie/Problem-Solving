c0de79f49b9727088f6e74ddb763b0b6b5fc56df
Shiva Shashank <shashanktech99@gmail.com>
2023-06-10 14:08:14 -0500

    Create pattern7.cpp

---
+++ Pattern Problems/pattern7.cpp
@@ -0,0 +1,12 @@
+for(int i = 0; i < n; i++){
+    for(int j = 0; j < n-i-1; j++){
+        cout << " ";
+    }
+    for(int j = 0; j < 2*i+1; j++){
+        cout << "*";
+    }
+    for(int j = 0; j < n-i-1; j++){
+        cout << " ";
+    }
+    cout << "\n";
+}
