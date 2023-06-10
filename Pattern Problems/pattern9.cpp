f9a71984c8dad8fd6b0f5b07a86a1cf682861065
Shiva Shashank <shashanktech99@gmail.com>
2023-06-10 16:46:10 -0500

    Create pattern9.cpp

---
+++ Pattern Problems/pattern9.cpp
@@ -0,0 +1,10 @@
+for(int i = 0; i <= (2*n)-1; i++){
+    int stars = i;
+    if(i > n){
+        stars = (2*n)-i;
+    }
+    for(int j = 0; j < stars; j++){
+        cout << '*';
+    }
+    cout << endl;
+}
