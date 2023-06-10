4fe5e81f0f1733b8f66e3f64a699cd7b9c15d87d
Shiva Shashank <shashanktech99@gmail.com>
2023-06-10 16:15:03 -0500

    Update pattern8.cpp

--- Pattern Problems/pattern8.cpp
+++ Pattern Problems/pattern8.cpp
@@ -1,11 +1,11 @@
-for(int i=0; i<n; i++){
-    for(int j=0;j<i;j++){
+for(int i = 0; i < n; i++){
+    for(int j = 0; j < i; j++){
         cout << " ";
     }
-    for(int j=0; j<2*n-(2*i+1);j++){
+    for(int j = 0; j < 2*n - (2*i+1); j++){
             cout << '*';
     }
-    for(int j=0; j<i; j++){
+    for(int j = 0; j < i; j++){
         cout << " ";
     }
     cout << "\n";
