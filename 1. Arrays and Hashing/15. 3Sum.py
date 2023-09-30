2fe6c48eca715bbeccf5372424467adfceae3900
Shiva Shashank <shashanktech99@gmail.com>
2023-09-30 15:27:12 -0500

    Update 15. 3Sum.py

--- 1. Arrays and Hashing/15. 3Sum.py
+++ 1. Arrays and Hashing/15. 3Sum.py
@@ -14,3 +14,31 @@ class Solution:
                 hashmap.add(nums[j])
         ans = list(st)
         return ans
+
+# Optimal Approach - O(n^2)
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        ans = []
+        for i in range(len(nums)):
+            if(i != 0 and nums[i-1] == nums[i]):
+                continue
+            j = i + 1
+            k = len(nums) - 1
+            while(j < k):
+                total_sum = nums[i] + nums[j] + nums[k]
+                if(total_sum < 0):
+                    j += 1
+                elif(total_sum > 0):
+                    k -= 1
+                else:
+                    temp = [nums[i], nums[j], nums[k]]
+                    ans.append(temp)
+                    j += 1
+                    k -= 1
+                    while(j < k and nums[j] == nums[j-1]):
+                        j += 1
+                    while(j < k and nums[k] == nums[k+1]):
+                        k -= 1
+        return ans
