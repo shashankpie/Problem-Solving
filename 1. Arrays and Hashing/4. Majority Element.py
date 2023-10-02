ac2dcb9a4a4b3ae57e3fa1c653700b2f1113faa3
Shiva Shashank <shashanktech99@gmail.com>
2023-10-02 15:24:33 -0500

    Create 4. Majority Element.py

---
+++ 1. Arrays and Hashing/4. Majority Element.py
@@ -0,0 +1,18 @@
+# Brute force Solution
+# Time complexity - O(n^2)
+# Space complexity - O(1)
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        res = []
+        for i in range(len(nums)):
+            if len(res) == 0 or res[0] != nums[i]:
+                cnt = 0
+                for j in range(len(nums)):
+                    if nums[i] == nums[j]:
+                        cnt += 1
+                if cnt > (len(nums) // 3):
+                    res.append(nums[i])
+            if len(res) == 2:
+                break
+        return res
