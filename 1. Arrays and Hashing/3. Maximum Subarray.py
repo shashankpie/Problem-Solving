2cfcbc338de71e35e4175898926a9b836b7e3d8e
Shiva Shashank <shashanktech99@gmail.com>
2025-01-15 19:16:22 -0600

    Update 3. Maximum Subarray.py with comments

--- 1. Arrays and Hashing/3. Maximum Subarray.py
+++ 1. Arrays and Hashing/3. Maximum Subarray.py
@@ -5,11 +5,13 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         maxi = nums[0]
-        sum_total = 0
+        sum_nums = 0
         for i in range(len(nums)):
-            sum_total = sum_total + nums[i]
-            if sum_total > maxi:
-                maxi = sum_total
-            if sum_total < 0:
-                sum_total = 0
+            sum_nums += nums[i]
+            if sum_nums > maxi:
+                maxi = sum_nums
+            # If sum of numbers is a negative number, we simply ignore
+            # And start from non-negative numbers by making sum as 0
+            if sum_nums < 0:
+                sum_nums = 0
         return maxi
